f1cf003d1dbf3d117d1f4983e42b0b2fcc80977c
Mitesh savaliya <msavaliya@plusonex.com>
2024-05-05 10:08:28 +0530

    shad cn added
    yet to remove unused call

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +11,10 @@ export default defineConfig({
   preview: {
     port: 5173,
     host: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
